e8755b5f46a1f32f8722edc7e1b8649d02954610
GeoffryAbiFarah <geoffry_abifarah@hotmail.com>
2022-01-15 19:22:36 +0200

    added promo service and home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DishService } from '../services/dish.service';
+import { PromotionService } from '../services/promotion.service';
+import { Dish } from '../shared/dish';
+import { Promotion } from '../shared/promotion';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  dish: Dish = {
+    id: '',
+    name: '',
+    image: '',
+    category: '',
+    featured: false,
+    label: '',
+    price: '',
+    description: '',
+    comments: []
+  };
+  promotion: Promotion = {
+    id: '',
+    name: '',
+    image: '',
+    label: '',
+    price: '',
+    featured: false,
+    description: ''
+  };
+
+  constructor(private dishService: DishService, private promoService: PromotionService ) {}
 
   ngOnInit(): void {
+    this.dish = this.dishService.getFeaturedDish();
+    this.promotion = this.promoService.getFeaturedPromotion();
   }
 
 }
