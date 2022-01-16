5c160da332192c1f6579b4ad161da1237622c149
GeoffryAbiFarah <geoffry_abifarah@hotmail.com>
2022-01-16 17:51:19 +0200

    added routing with params

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
+
 export class DishdetailComponent implements OnInit {
 
-  @Input()
   dish: Dish = {
       id: '',
       name: '',
@@ -21,9 +24,14 @@ export class DishdetailComponent implements OnInit {
       comments: []
   }
 
-  constructor() { }
+  constructor(private dishService: DishService, private location: Location, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
   }
 
+  goBack(): void{
+    this.location.back();
+  }
 }
